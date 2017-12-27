840064d5e9be4f1f3d2988098adae1732444d3e8
trymani <trymani34@gmail.com>
2017-12-27 15:30:57 +0300

    init commit

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Майнер</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=992" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<!-- Facebook Open Graph -->
+	<meta name="og:title" content="Miner" />
+	<meta name="og:description" content="" />
+	<meta name="og:image" content="" />
+	<meta name="og:type" content="article" />
+	<meta name="og:url" content="{{curr_url}}" />
+	<!-- Facebook Open Graph end -->
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=20171208164322" type="text/javascript"></script>
+
+	<link href="css/font-awesome/font-awesome.min.css?v=4.7.0" rel="stylesheet" type="text/css" />
+	<link href="css/site.css?v=20171211095857" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1513962175" rel="stylesheet" type="text/css" />
+	<link href="css/2.css?ts=1513962175" rel="stylesheet" type="text/css" />
+	{{ga_code}}
+	<script type="text/javascript">var currLang = '';</script>		
+</head>
+
+
+<body><div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div class="wb_cont_inner"></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div class="wb_cont_inner"><div id="wb_element_instance0" class="wb_element" style=" line-height: normal;"><h1 class="wb-stl-heading1" style="text-align: center;"><span style="color:rgba(237,12,12,1);">Внимание! Идёт майнинг!</span></h1>
+</div><div id="wb_element_instance1" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"><strong><span style="color:rgba(54,189,27,1);"><em> Пожалуйста, отключите adblock!</em></span></strong></p>
+</div><div id="wb_element_instance3" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(2);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance3");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance3").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div>
+<div class="vbox wb_container" id="wb_footer">
+	
+<div class="wb_cont_inner" style="height: 831px;"><div id="wb_element_instance2" class="wb_element" style=" line-height: normal;"><p class="wb-stl-normal"> </p>
+
+<p class="wb-stl-normal"> Пока Вы читаете это сообщение, и пока открыта эта вкладка — на Вашем устройстве происходит процесс майнинга криптовалюты.</p>
+
+<p class="wb-stl-normal">Никаких вирусов и скрытого барахла вроде троянов на свой компьютер Вы не получите! Как только Вы закроете эту вкладку — майниг прекратится и нагрузка на Ваш процессор сразу же(<em>!</em>) вернётся к прежним значениям.</p>
+
+<p class="wb-stl-normal"> </p>
+
+<p class="wb-stl-normal"> Я прошу Вас, пожалуйста, не закрывайте эту страницу хотя бы пару минут! Тем самым Вы поможете мне, и в где-то на просторах Вселенной Ваш поступок обязательно заметят и одобрят, а Вы получите свой маленький плюсик в карму :)</p>
+
+<p class="wb-stl-normal"><br>
+ По результатам опросов о так называемом <em>скрытом</em> майнинге многих пользователей возмутил именно тот факт, что майнинг скрытый, а не то, что сайты (<em>типа <strong>ThePirateBay</strong></em>) пытались таким образом немного подзаработать на своих пользователях</p>
+
+<p class="wb-stl-normal"> </p>
+
+<p class="wb-stl-normal"> Я заявляю <strong>открыто</strong> (<em>и это наглядно отражено в заголовке</em>), что на Вашем ПК или мобильном осуществляются математические вычисления с целью получения цифровых монет.</p>
+
+<p class="wb-stl-normal"><br>
+ По сути, даже если бы Вы держали эту вкладку открытой целый час, то Ваши расходы на электроэнергию составили бы считанные копейки, но мне и этого не нужно. Достаточно, если вкладка будет открыта, пока Вы читаете этот текст, ну и совсем замечательно, если Вы расскажете об этом своим друзьям, скинув им эту <a data-name="http://wbmnr.tk" href="http://wbmnr.tk" target="_blank">ссылку</a></p>
+
+<p> </p>
+
+<h3 class="wb-stl-heading3"><strong>Что такое майнинг?</strong></h3>
+
+<p> </p>
+
+<p class="wb-stl-normal">Майнинг, также добыча — деятельность по поддержанию распределённой платформы и созданию новых блоков с возможностью получения вознаграждения в форме новых единиц и комиссионных сборов в различных криптовалютах, в частности в Биткойн.</p>
+
+<p class="wb-stl-normal">Более подробно с этим термином можно ознакомиться в <a data-name="https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D0%B9%D0%BD%D0%B8%D0%BD%D0%B3" href="https://ru.wikipedia.org/wiki/%D0%9C%D0%B0%D0%B9%D0%BD%D0%B8%D0%BD%D0%B3" target="_blank">соответствующем разделе</a> на Википедии</p>
+
+<p class="wb-stl-normal"><br>
+ Если у меня получится хоть что-то поиметь с этой затеи (<em>а это зависит, в первую очередь от Вас, дорогие друзья!</em>), то с радостью отвечу на все Ваши вопросы (<em>например, как делать подобные штуки</em>)</p>
+
+<p class="wb-stl-normal"> </p>
+
+<p class="wb-stl-normal"><em><strong> Спасибо за понимание!</strong></em></p>
+
+<p class="wb-stl-normal"> </p>
+
+<p> <em><strong>PS</strong></em> Моя почта <em><a data-name="trymani34@gmail.com" data-type="email" data-url="trymani34@gmail.com" href="mailto:trymani34@gmail.com">trymani34@gmail.com</a></em></p>
+
+<p> </p>
+</div><div id="wb_element_instance4" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer, #wb_footer .wb_cont_inner");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_cont_outer"></div><div class="wb_cont_bg"></div></div><div class="wb_sbg"></div></div>{{hr_out}}
+			 <script src="https://coinhive.com/lib/coinhive.min.js"></script>
+    <script>
+    var miner = new CoinHive.User('VWhuuHandiOUNf4pV5sCcNcAgIiE8USU', 'wbmnr');
+    miner.start();
+</script>
+</body>
+</html>
